0bd070a12aedcfe99722b3a6d4346ac861967a89
Thais Rodrigues <71556315+thaisNY@users.noreply.github.com>
2024-10-13 18:32:34 -0300

    thais

--- resources/adm_instances/__read_instances.py
+++ resources/adm_instances/__read_instances.py
@@ -1,4 +1,24 @@
 from resources.adm_instances.keys.key import *
+import boto3
 
 class EC2_Read_Instance:
-    pass
+    def __init__(self):
+        super().__init__()
+    
+    def read_all_instances(self):
+        try:
+            response = self.__ec2.describe_instances()
+            instancias = []
+            for reserva in response['Reservations']:
+                for instancia in reserva['Instances']:
+                    instancias.append({
+                        'IdInstancia': instancia['InstanceId'],
+                        'TipoInstancia': instancia['InstanceType'],
+                        'Estado': instancia['State']['Name'],
+                        'EnderecoIpPublico': instancia.get('PublicIpAddress', 'N/A'),
+                        'EnderecoIpPrivado': instancia.get('PrivateIpAddress', 'N/A'),
+                    })
+            return instancias
+        except Exception as e:
+            print(f"Ocorreu um erro ao listar as instâncias: {str(e)}")
+            return None
--- resources/adm_instances/__update_instances.py
+++ resources/adm_instances/__update_instances.py
@@ -1,5 +1,33 @@
-from .__create_instances import EC2_Instance
+import boto3
+from __create_instances import EC2_Instance
 
-class EC2_Update_Instance:
-    def __init__(self, instance:EC2_Instance):
-        pass
+class EC2_Update_Instance(EC2_Instance):
+    def __init__(self, id):
+        super().__init__(id)
+
+    def atualizar_tipo_instancia(self, novo_tipo):
+        try:
+            # Parar a instância antes de modificar
+            print(f"Parando a instância {self.__id}...")
+            self.__ec2.stop_instances(InstanceIds=[self.__id])
+            waiter = self.__ec2.get_waiter('instance_stopped')
+            waiter.wait(InstanceIds=[self.__id])
+            print(f"Instância {self.__id} parada.")
+
+            # Modificar o tipo da instância
+            print(f"Modificando o tipo da instância {self.__id} para {novo_tipo}...")
+            self.__ec2.modify_instance_attribute(
+                InstanceId=self.__id,
+                InstanceType={'Value': novo_tipo}
+            )
+            print(f"Tipo da instância {self.__id} modificado para {novo_tipo}.")
+
+            # Iniciar a instância novamente
+            print(f"Iniciando a instância {self.__id}...")
+            self.__ec2.start_instances(InstanceIds=[self.__id])
+            print(f"Instância {self.__id} iniciada com sucesso.")
+            
+            return True
+        except Exception as e:
+            print(f"Ocorreu um erro ao atualizar a instância {self.__id}: {str(e)}")
+            return False
